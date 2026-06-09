e54a54868e19e4a66cfe02589425ce4775c1820f
SinghAmit <amitsinghh9208@gmail.com>
2026-06-09 18:22:29 +0530

    Implement theme toggle and load saved theme
    Added theme toggle functionality and saved theme loading.

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+// Theme toggle
+function toggleTheme() {
+  document.body.classList.toggle("light");
+
+  if (document.body.classList.contains("light")) {
+    localStorage.setItem("theme", "light");
+  } else {
+    localStorage.setItem("theme", "dark");
+  }
+}
+
+// Load saved theme
+window.onload = function () {
+  const savedTheme = localStorage.getItem("theme");
+
+  if (savedTheme === "light") {
+    document.body.classList.add("light");
+  }
+};
+
 // Wait until page loads
 document.addEventListener("DOMContentLoaded", function() {
 
